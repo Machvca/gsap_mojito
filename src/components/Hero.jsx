e6742f4cd3a05f2ab334ae872d3c592d7581ccef
Jorge Machuca <jjorgemachuca@gmail.com>
2025-07-24 17:32:33 +0200

    hero video animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,34 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const videoTimelineRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
 
-    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'))
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
     gsap.from(heroSplit.chars, {
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.06
-    })
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
 
     gsap.from(paragraphSplit.lines, {
-        opacity:0,
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.06,
-        delay: 1,
-    })
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
 
     gsap
       .timeline({
@@ -35,11 +41,30 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    //video animation timeline
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title">MOJITO</h1>
+        <h1 className="title">MACHU</h1>
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
@@ -70,9 +95,17 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
-
 
 export default Hero;
